011f76a47ee6940be0f0526f94397215f15c7af5
Jace Cotton <jxcotton@texaschildrens.org>
2022-03-29 15:42:12 -0500

    docs: Add inline documentation for script bundle

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,7 @@
+/**
+ * Brings in each module for bundling. No configuration or scripting happens
+ * here.
+ */
+
 import Accordion from "./components/Accordion.js";
 import Hero from "./components/Hero.js";
