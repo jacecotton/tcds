75ace5741e6a85e07e87d4ee2b5ea5aa41ef1790
Jace Cotton <jxcotton@texaschildrens.org>
2022-03-31 15:14:39 -0500

    feat: #3 Add Carousel component

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,3 +6,4 @@
 import Accordion from "./components/Accordion.js";
 import Hero from "./components/Hero.js";
 import Tabs from "./components/Tabs.js";
+import Carousel from "./components/Carousel.js";
