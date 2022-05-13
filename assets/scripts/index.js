9ea44ee82c7bae91697fceaa4c4978813abd8df1
Jace Cotton <jxcotton@texaschildrens.org>
2022-05-12 20:51:20 -0500

    fix(scripts): Conditionally load cqfill

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -9,3 +9,9 @@ import Hero from "./components/Hero.js";
 import Tabs from "./components/Tabs.js";
 import Carousel from "./components/Carousel.js";
 import Notification from "./components/Notification.js";
+
+if(!("container" in document.documentElement.style) && document.querySelectorAll(".is-self-responsive").length > 0) {
+  const script = document.createElement("script");
+  script.src = "https://unpkg.com/container-query-polyfill/cqfill.iife.min.js";
+  document.head.appendChild(script);
+}
