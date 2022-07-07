13b01ec4f728cdc7ff95df4ea146966452518603
Jace Cotton <jxcotton@texaschildrens.org>
2022-07-07 12:27:13 -0500

    refactor(scripts): Simplification

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -4,14 +4,14 @@
  * here.
  */
 
-import Accordion from "./components/Accordion.js";
-import Dialog from "./components/Dialog.js";
+import "./components/Accordion.js";
+import "./components/Dialog.js";
 import "./components/Header.js";
-import Hero from "./components/Hero.js";
-import MegaMenu from "./components/MegaMenu.js";
-import Notification from "./components/Notification.js";
-import Tabs from "./components/Tabs.js";
-import Carousel from "./components/Carousel.js";
+import "./components/Hero.js";
+import "./components/MegaMenu.js";
+import "./components/Notification.js";
+import "./components/Tabs.js";
+import "./components/Carousel.js";
 
 if(!("container" in document.documentElement.style) && document.querySelectorAll(".is-self-responsive").length > 0) {
   const script = document.createElement("script");
