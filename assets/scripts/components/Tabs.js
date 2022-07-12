d3da16ca85800113857626b8f6725ea13f488177
Jace Cotton <jxcotton@texaschildrens.org>
2022-07-12 15:50:53 -0500

    fix(components): Major Tabs markup refactor

--- assets/scripts/components/Tabs.js
+++ assets/scripts/components/Tabs.js
@@ -1,171 +1,106 @@
 import Component from "@tcds/utilities/Component.js";
+import slugify from "@tcds/utilities/slugify.js";
 
-/**
- * Tabs component.
- *
- * Vocabulary:
- * 1. "tabs" or "tab buttons": The labeled buttons clicked to activate a panel.
- *    These must have `[role=tab]` and `[aria-controls]` attributes.
- * 2. "tablist": The element containing all tabs in a row. This has a
- *    `[role=tablist]`.
- * 3. "panels" or "tabpanels": The content associated with each tab. These have
- *    a `[role=tabpanel]`.
- *
- * Accessibility and usability requirements:
- * 1. Source order is (all tabs) + (all tabpanels), but tabbing order needs to
- *    be (tab button) -> (associated tabpanel), with sibling tabs focusable via
- *    left and right arrow keys.
- * 2. State is indicated with the `[aria-expanded]` attribute on tabs, and the
- *    `[hidden]` attribute on panels.
- * 3. Association between tabs and panels is established via
- *    `[aria-controls=panel id]` on tabs, and `[aria-labelledby=tab id]` on
- *    panels.
- *
- * @property {object} props - Tabs settings.
- * @property {boolean} [props.hideAll=false] - Whether to hide all tabs
- * initially.
- * @property {boolean} [props.keepPanelVisibility=false] - Whether to add the
- * `[hidden]` attribute to panels when they are deselected. Only set this prop
- * to `true` if the panels are going to be hidden through some other mechanism
- * (e.g. scrolling out of view in the case of the Carousel, which extends this
- * component).
- */
 export default class Tabs extends Component {
   constructor(element, props) {
     super(element, props);
 
-    // Get relevant DOM elements, converting them to normal arrays from
-    // NodeLists so that array methods (like `[].indexOf`) are available.
-    this.tabs = Array.from(this.element.querySelectorAll("[role=tab]"));
-    this.tablist = this.element.querySelector("[role=tablist]");
-    this.panels = Array.from(this.element.querySelectorAll("[role=tabpanel]"));
-    this.panelsContainer = this.element.querySelector("[data-component-part=tabpanels]");
+    this.preprocessDOM();
 
-    // Set the first tab to active (as long as the `hideAll` prop is not `true`;
-    // otherwise, set active tab to `null` so that no tabs are active).
-    this.state.activeTab = (this.props.hideAll !== true) ? this.tabs[0] : null;
+    this.state.activeTab = this.props.hideAll !== true ? this.tabs[0] : null;
 
-    // Define a key map for what to do when certain keys are pressed while
-    // keyboard focus is on a tab.
-    const keymap = {
-      // Go to (and focus on) next tab on right arrow key press.
-      "ArrowRight": () => {
-        this.state.activeTab = this.getNextTab();
-        this.state.activeTab.focus();
-      },
-
-      // Go to (and focus on) previous tab on left arrow key press.
-      "ArrowLeft": () => {
-        this.state.activeTab = this.getPreviousTab();
-        this.state.activeTab.focus();
-      },
-    };
-
-    // Set up event listeners.
     this.tabs.forEach((tab) => {
-      // Activate tab when clicked.
       tab.addEventListener("click", () => {
         this.state.activeTab = tab;
       });
 
-      // Listen for navigation arrows when a tab button is focused.
       tab.addEventListener("keydown", (key) => {
-        // If the key code for the triggered event has a match in the key map,
-        // call its associated function.
-        keymap[key.code] && keymap[key.code]();
+        if(key.code === "ArrowRight") {
+          this.state.activeTab = this.getNextTab();
+          this.state.activeTab.focus();
+        } else if(key.code === "ArrowLeft") {
+          this.state.activeTab = this.getPreviousTab();
+          this.state.activeTab.focus();
+        }
       });
     });
   }
 
-  // Update DOM after state change.
+  preprocessDOM() {
+    this.panels = Array.from(this.element.querySelectorAll("section"));
+    this.tabs = [];
+
+    this.tablist = document.createElement("div");
+    this.tablist.setAttribute("role", "tablist");
+    this.tablist.classList.add("Tabs__tablist");
+
+    this.viewport = document.createElement("div");
+    this.viewport.classList.add("Tabs__viewport");
+
+    this.panels.forEach((panel) => {
+      const heading = panel.querySelector("h2, h3, h4, h5, h6");
+      const id = slugify(heading.innerText);
+
+      panel.setAttribute("role", "tabpanel");
+      panel.setAttribute("id", `${id}-panel`);
+      panel.setAttribute("aria-labelledby", `${id}-tab`);
+      panel.classList.add("Tabs__panel");
+      this.viewport.appendChild(panel);
+
+      const tab = document.createElement("button");
+      tab.setAttribute("role", "tab");
+      tab.setAttribute("id", `${id}-tab`);
+      tab.setAttribute("aria-controls", `${id}-panel`);
+      tab.classList.add("Tabs__tab");
+      tab.textContent = heading.innerText;
+
+      heading.remove();
+      this.tablist.append(tab);
+      this.tabs.push(tab);
+    });
+
+    this.element.prepend(this.tablist);
+    this.element.append(this.viewport);
+  }
+
   sync() {
     return {
-      // Handler for `activeTab` state change.
       activeTab: () => {
         this.tabs.forEach((tab) => {
-          // Indicate ARIA "expanded" state according to whether the current tab
-          // in the loop is the active tab.
-          tab.setAttribute("aria-expanded", (tab === this.state.activeTab));
-
-          // Because each tab is accessible via arrow key, we want all inactive
-          // tabs to not be accessible via the Tab key, instead making the tab's
-          // asssociated content next in the tabbing order. If the current tab is
-          // the active tab (or if there is no active tab), then we'll keep
-          // keyboard accessibility via the Tab key by setting its tabindex to 0.
+          tab.setAttribute("aria-expanded", tab === this.state.activeTab);
           tab.setAttribute("tabindex", (tab === this.state.activeTab || !this.state.activeTab) ? "0" : "-1");
         });
 
-        if(!this.props.keepPanelVisibility) {
-          this.panels.forEach((panel) => {
-            // Hide panel if it is not the active panel (and vice versa).
-            panel.hidden = !(panel === this.getPanelByTab(this.state.activeTab));
-          });
-        }
+        this.panels.forEach((panel) => {
+          if(this.props.keepPanelVisibility !== true) {
+            panel.hidden = panel !== this.getPanelByTab(this.state.activeTab);
+          }
+        });
 
-        // Fire a noop scroll event to trigger lazy-loading (in case it exists).
         window.dispatchEvent(new Event("scroll"));
       },
     };
   }
 
-  /** Utilities */
-
-  /**
-   * Get the next tab after the given one.
-   *
-   * @param {HTMLElement} [relativeTab=this.state.activeTab] - The tab relative
-   * to which you want to get the next tab (defaults to the currently active
-   * tab).
-   * @returns {HTMLElement} - A tab.
-   */
   getNextTab(relativeTab = this.state.activeTab) {
-    // If the given tab is the last in the order...
-    return (relativeTab === this.tabs[this.tabs.length - 1])
-      // Return the first tab.
+    return relativeTab === this.tabs[this.tabs.length - 1]
       ? this.tabs[0]
-      // Otherwise, return the tab one index ahead of the given one.
       : this.tabs[this.tabs.indexOf(relativeTab) + 1];
   }
 
-  /**
-   * Get the previous tab before the given one.
-   *
-   * @param {HTMLElement} [relativeTab=this.state.activeTab] - The tab relative
-   * to which you want to get the previous tab (defaults to the currently active
-   * tab).
-   * @returns {HTMLElement} - A tab.
-   */
   getPreviousTab(relativeTab = this.state.activeTab) {
-    // If the given tab is the first in the order...
-    return (relativeTab === this.tabs[0])
-      // Return the last tab.
+    return relativeTab === this.tabs[0]
       ? this.tabs[this.tabs.length - 1]
-      // Otherwise, return the tab one index behind the given one.
       : this.tabs[this.tabs.indexOf(relativeTab) - 1];
   }
 
-  /**
-   * Get the panel controlled by the given tab.
-   *
-   * @param {HTMLElement} tab - The tab for which you want to get the associated
-   * panel.
-   * @returns {HTMLElement} - A panel.
-   */
   getPanelByTab(tab) {
-    // Check whether the passed `tab` argument is an actual tab (an HTML element
-    // with a `[role=tab]`).
-    return (tab instanceof HTMLElement && tab.getAttribute("role") === "tab")
-      // If so, return the panel with an ID matching the `[aria-controls]`
-      // attribute of the tab.
-      ? this.panels.find(panel => tab.getAttribute("aria-controls") === panel.id)
-      // Otherwise, return `null`.
-      : null;
+    return tab !== null && this.panels.find((panel) => tab.getAttribute("aria-controls") === panel.id);
   }
 }
 
-// Attach component.
 document.querySelectorAll("[data-component=Tabs]").forEach((instance) => {
   instance && new Tabs(instance, {
-    hideAll: instance.classList.contains("Tabs--hide-all"),
+    hideAll: instance.hasAttribute("data-hide-all"),
   });
 });
