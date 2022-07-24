58953f4450ecbf9a29e631ec01e66057c16cf0a5
Jace Cotton <jxcotton@texaschildrens.org>
2022-07-24 04:27:17 -0500

    Button todo note

--- assets/scripts/components/Button.js
+++ assets/scripts/components/Button.js
@@ -1,5 +1,15 @@
 import WebComponent from "@tcds/WebComponent/WebComponent.js";
 
+/**
+ * @todo Move icon cache to localStorage. The current "cache" isn't really a
+ * cache because it's not persistent across different instances and page loads.
+ * Moving to localStorage is better as it acts like a genuine cache, allowing
+ * for icon reuse across different component instances, page loads, and renders.
+ * It also prevents scope pollution better than the doublescored prefix.
+ * @todo Figure out a better way to fetch icons. UNPKG isn't intended for
+ * production. Preferably pull from local copy somehow. Also create <tcds-icon>
+ * component to handle fetching, caching, etc.
+ */
 window.__TCDS_ICON_CACHE = {};
 
 export default class Button extends WebComponent {
