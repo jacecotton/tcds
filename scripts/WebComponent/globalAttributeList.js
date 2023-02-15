8e068d07c881458e325f6a7a1af131a8a625b3bc
Jace Cotton <jxcotton@texaschildrens.org>
2023-02-15 10:37:09 -0600

    refactor(scripts): Split global attribute list into separate file

---
+++ scripts/WebComponent/globalAttributeList.js
@@ -0,0 +1,18 @@
+/**
+ * This isn't necessarily comprehensive, rather just a blacklist of attributes
+ * we don't want being registered as props.
+ */
+
+const globalAttributesFull = [
+  "accesskey", "autocapitalize", "autofocus", "class", "contenteditable",
+  "dir", "draggable", "hidden", "href", "id", "inert", "is", "lang",
+  "name", "onblur", "onfocus", "onclick", "onchange", "ondrop", "onerror",
+  "onload", "onscroll", "onselect", "onsubmit", "onwheel", "part", "role",
+  "slot", "spellcheck", "src", "style", "tabindex", "title", "translate",
+];
+
+const globalAttributesPartial = [
+  "aria-", "data-", "onmouse", "onpointer", "ondrag", "onkey"
+];
+
+export { globalAttributesFull, globalAttributesPartial };
