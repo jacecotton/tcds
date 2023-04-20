aab1a3db82c830622f6cda62c26542195e5a8892
jacecotton <jacedc98@gmail.com>
2023-04-20 15:08:18 -0500

    update

--- components/section/index.js
+++ components/section/index.js
@@ -2,6 +2,55 @@ import WebComponent from "../../utilities/WebComponent/WebComponent.js";
 import styles from "./style.css";
 
 export default class Section extends WebComponent(HTMLElement) {
+  #has(...slots) {
+    return !!this.querySelector([...slots].map(slot => `[slot=${slot}]`).join(", "));
+  }
+
+  get template() {
+    return /* html */`
+      <section>
+        <div part="background" class="${[...this.classList].filter(className => className.startsWith("bg-"))}" data-theme="${this.dataset.theme}">
+          ${this.#has("background") ? `<slot name="background"></slot>` : ``}
+        </div>
+
+        <div part="primary" class="${!this.#has("tertiary") ? "fill-width" : ""} ${!this.#has("secondary") ? "fill-height" : ""}">
+          ${this.#has("overline") || this.#has("heading") || this.#has("subheading") ? /* html */`
+            <hgroup>
+              <slot name="overline"></slot>
+              <slot name="heading"></slot>
+              <slot name="subheading"></slot>
+            </hgroup>
+          ` : ``}
+
+          <slot name="primary"></slot>
+          <slot></slot>
+
+          ${this.#has("cta") ? /* html */`
+            <slot name="cta"></slot>
+          ` : ``}
+        </div>
+
+        ${this.#has("secondary") || (this.#has("image") && !this.#has("tertiary")) ? /* html */`
+          <div part="secondary" ${!this.#has("tertiary") ? `class="fill-width"` : ""}>
+            <slot name="secondary"></slot>
+            ${this.#has("image") ? /* html */`
+              <!-- LEGACY -->
+              <slot name="image"></slot>
+            ` : ``}
+          </div>
+        ` : ``}
+
+        ${this.#has("tertiary", "image") ? /* html */`
+          <div part="tertiary" class="fill-height">
+            <slot name="tertiary"></slot>
+            <!-- LEGACY -->
+            <slot name="image"></slot>
+          </div>
+        ` : ``}
+      </section>
+    `;
+  }
+
   constructor() {
     super();
     this.shadowRoot.adoptedStyleSheets = [styles];
@@ -9,48 +58,16 @@ export default class Section extends WebComponent(HTMLElement) {
 
   connectedCallback() {
     this.update();
-  }
 
-  get template() {
-    // const hasPrimary = !!this.querySelector("[slot=primary]");
-    const hasSecondary = !!this.querySelector("[slot=secondary]");
-    const hasTertiary = !!this.querySelector("[slot=tertiary]");
-    // const hasOverline = !!this.querySelector("[slot=overline]");
-    // const hasHeading = !!this.querySelector("[slot=heading]");
-    // const hasSubheading = !!this.querySelector("[slot=subheading]");
-
-    return /* html */`
-      <section>
-        <div part="background" class="${[...this.classList].filter(className => className.startsWith("bg-"))}" data-theme="${this.dataset.theme}">
-          <slot name="background"></slot>
-        </div>
-
-        <div part="primary" class="${!hasTertiary ? "fill-width" : ""} ${!hasSecondary ? "fill-height" : ""}">
-          PRIMARY REGION<br>
-          (heading, subhead, overline, CTA)
-          <slot></slot>
-        </div>
-
-        ${hasSecondary ? /* html */`
-          <div part="secondary" ${!hasTertiary ? `class="fill-width"` : ""}>
-            <slot name="secondary"></slot>
-          </div>
-        ` : ``}
-
-        ${hasTertiary ? /* html */`
-          <div part="tertiary" class="fill-height">
-            <slot name="tertiary"></slot>
-          </div>
-        ` : ``}
-      </section>
-    `;
+    if(this.#has("background") && this.getAttribute("data-theme") !== "light") {
+      this.setAttribute("data-theme", "dark");
+    }
   }
 
   mountedCallback() {
-    this.shadowRoot.querySelectorAll("slot").forEach((slot) => {
-      slot.addEventListener("slotchange", () => {
-        this.update({[slot.name]: slot.assignedNodes()});
-      });
+    this.shadowRoot.addEventListener("slotchange", (event) => {
+      console.log(event);
+      this.update({[event.target.name]: event.target.assignedNodes()});
     });
   }
 }
