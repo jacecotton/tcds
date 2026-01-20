f109f86f86e832d7d0395596488d3c1d616908da
Jace Cotton <jxcotton@texaschildrens.org>
2026-01-20 11:00:20 -0600

    Remove unneeded accordion styles

--- src/components/accordion/accordion.styles.js
+++ src/components/accordion/accordion.styles.js
@@ -8,21 +8,8 @@ export default css`
 
   [part="controls"] {
     display: flex;
-    gap: 1rem;
     justify-content: end;
     border-bottom: 1px solid var(--tcds-accordion-border-color);
     padding: 0 0 .5rem;
-  }
-
-  [part="open-all"],
-  [part="close-all"] {
-    appearance: none;
-    background: none;
-    border: 0;
-    color: inherit;
-    font-family: var(--tcds-font-family-ui);
-    font-weight: var(--tcds-font-weight-semibold);
-    display: flex;
-    gap: 1ch;
   }
 `;
